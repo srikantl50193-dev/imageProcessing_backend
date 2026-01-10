9075be3d52dfc6e926976fb989813a595df32ea6
Srikant <srikant.l@repintel.com>
2026-01-09 19:10:47 -0500

    Update CORS to allow new frontend URL: https://image-processing-e9o9uni0k-srikant-lakshminarayans-projects.vercel.app

--- api/test.js
+++ api/test.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   // Enable CORS
-  res.setHeader('Access-Control-Allow-Origin', 'https://image-processing-l7ovo90tw-srikant-lakshminarayans-projects.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', 'https://image-processing-e9o9uni0k-srikant-lakshminarayans-projects.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
 
